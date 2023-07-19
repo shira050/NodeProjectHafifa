3c4e1743928638e1d9d7ecc1f00de181e1de1d1e
shira <s0504173725@gmail..com>
2023-07-19 17:26:25 +0300

    final project-without env

--- NodeServer/routes/index.js
+++ NodeServer/routes/index.js
@@ -9,77 +9,89 @@ const fsPromise = require('fs/promises')
 let intervalId;
 const pathFolder = process.env.FOLDER_URL || "C:/Users/Shira/Documents/nodeProject/NodeServer/public/";
 const pathServerWriteFunction = process.env.EXSPRESS_SERVER_URL || "http://localhost:3000/";
+const maxBadRequest = process.env.MAX_SENDING || 10;
 
+//create interval for rotate
 if (intervalId) {
   clearInterval(intervalId);
 }
 intervalId = setInterval(async () => {
-  debugger
   await readAllFolderText();
 }, 10000);
 
-//let isProcessing = false; // Flag to track if readAllFolderText() is currently running
+//function to algorithm
+const isNum = (extentionStr) => {
+  return /^\d+$/.test(extentionStr);
+}
 
-// async function intervalFunction() {
-//   if (!isProcessing) {
-//     isProcessing = true;
-//     try {
-//       debugger;
-//       await readAllFolderText();
-//     } catch (error) {
-//       // Handle any errors that occur during readAllFolderText()
-//     } finally {
-//       isProcessing = false;
-//     }
-//   }
-// }
+const getLastExeption = (file) => {
+  return file.substring((file.lastIndexOf(".") + 1));
+}
 
-//  intervalId = setInterval(intervalFunction, 10000);
-
-
-
-const readAllFolderText = async () => {
-
-  debugger
-  try {
-    fs.readdirSync(pathFolder).forEach(async file => {
-      let extension = file.substring(file.lastIndexOf("."));
-      console.log(extension);
-      if (extension != ".copied") {
-        debugger
-        let text = await fsPromise.readFile(pathFolder + file.toString(), { encoding: 'utf-8' });
-        // let resWriteFile =
-         await doApiMethod(pathServerWriteFunction + 'write/' + file, 'POST', { "content": text });
-      //   console.log(resWriteFile);
-      //  if(resWriteFile){
-        let copiedName = file + ".copied";
-        await fs.renameSync(pathFolder + file, pathFolder + copiedName);
-        console.log(file);
-      //  }
-      }
-    });
+const renameFileByBadReqest = async (file) => {
+  const lastExtension = getLastExeption(file);
+  let countBadSend = 0;
+  let newFileName = "";
+  if (isNum(lastExtension&&lastExtension)) {
+    countBadSend = (Number(lastExtension) + 1);
+    newFileName = file.replace("." + lastExtension, "." + countBadSend);
   }
-  catch (err) {
-    console.log(err + "axios error- file already exist");
+  else {
+    countBadSend = 1;
+    newFileName = file + "." + (countBadSend.toString());
+  }
+  await fs.renameSync(pathFolder + file, pathFolder + newFileName);
+}
+const renameFileDeleteCountReqest =  (file,lastExtension) => {
+  if (isNum(lastExtension)) {
+    return nameWithoutLastExtention = file.substring(0, (file.lastIndexOf(".")));  
+  }
+  return file;
+}
+
+const renameFileByCopy = async (file) => {
+  let fileNameWithoutCount=renameFileDeleteCountReqest(file,getLastExeption(file))
+  let text = await fsPromise.readFile(pathFolder + file.toString(), { encoding: 'utf-8' });
+  let res = await axios.post(`${pathServerWriteFunction}write/${fileNameWithoutCount}`, { "content": text });
+  console.log(res);
+  if (res && res.status == 200) {
+    copiedName = file.replace( getLastExeption(file), "copied");
+    await fs.renameSync(pathFolder + file, pathFolder + copiedName);
+  } else {
+    console.log("copy error");
   }
 }
 
-const doApiMethod = async (_url, _method, _body = {}) => {
-  try {
-    let resp = await axios({
-      url: _url,
-      method: _method,
-      data: _body,
-    });
-    debugger
-    return resp;
-  } catch (err) {
-    // return err
-    // console.error(err);
-    throw err
+const checkMaxRequestLimit = (file) => {
+  const lastExtension = getLastExeption(file);
+  if (isNum(lastExtension)) {
+    console.log("num reqest: ", lastExtension, "from ", maxBadRequest);
+    return Number(lastExtension) < maxBadRequest;
   }
-};
+  return true;
+}
+//main algorithm
+const readAllFolderText = async () => {
+  let currentFile;
+  var files = await fs.readdirSync(pathFolder);
+  const onlyFiles = files.filter(de => !de.isDirectory()).map(de => `${dirPath}/${de.name}`);
+  await Promise.all(onlyFiles.map(async (file) => {
+    try {
+      currentFile = file;
+      let extension = getLastExeption(file);
+      if (extension != "copied" && checkMaxRequestLimit(file)) {
+        await renameFileByCopy(file);
+      }
+    } catch (err) {
+      console.log(err);
+      await renameFileByBadReqest(currentFile);
+    }
+  }));
+}
+
+//router function-the same like main algorithm just 1 time
 router.get("/readFolderFiles", (req, res) => {
   readAllFolderText(req, res);
 })
+
 module.exports = router;
--- expressServer/routes/index.js
+++ expressServer/routes/index.js
@@ -6,13 +6,12 @@ const router = express.Router();
 router.get("/", (req, res) => {
   res.status(200).json({ msg: "index 3000 work" })
 })
-router.post("/read/readMultipleFiles", async (req, res) => {debugger
+router.post("/read/readMultipleFiles", async (req, res) => {
   if (!req.body.arr) {
     res.status(400).json({ msg: "you have to send arr with pethes" })
   }
   let allFilesText = []; debugger
   for (let index = 0; index < req.body.arr.length; index++) {
-    debugger
     const currentFilePath = req.body.arr[index];
     allFilesText[index] = (await readFile(currentFilePath));
 
@@ -20,7 +19,6 @@ router.post("/read/readMultipleFiles", async (req, res) => {debugger
   res.status(200).json({ msg: allFilesText })
 })
 router.get("/read/:filePath([\\w \\W]+)", async (req, res) => {
-  debugger
   let filePath = req.params.filePath;
   console.log(filePath);
   let fileText = await readFile(filePath);
@@ -28,11 +26,10 @@ router.get("/read/:filePath([\\w \\W]+)", async (req, res) => {
 })
 
 router.post("/write/:filePath([\\w \\W]+)", async (req, res) => {
-  debugger
-  let filePath = req.params.filePath;
+    let filePath = req.params.filePath;
   let content = req.body.content;
-  const path = 'C:/Users/Shira/Documents/nodeProject/expressServer/public/';
-//  const path = process.env.EXSPRESS_SERVER_URL || "http://localhost:3000/public";
+  const path =process.env.EXSPRESS_SERVER_URL || 'C:/Users/Shira/Documents/nodeProject/expressServer/public/';
+  console.log(path);
   let resFileText = await writeToFile(path+filePath, content);
   console.log(resFileText);
   if(resFileText){
